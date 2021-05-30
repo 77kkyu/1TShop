c8b112002181cb895199d58e1c78db6eae23c71a
sosoKim9966 <sosokim9966@gmail.com>
2021-05-31 01:03:09 +0900

    깃 이그노어 설정 변경

--- src/main/webapp/resources/static/assets/libs/jquery-sparkline/build.js
+++ src/main/webapp/resources/static/assets/libs/jquery-sparkline/build.js
@@ -5,7 +5,6 @@ var package = require('./package');
 var files = ['header.js', 'defaults.js', 'utils.js', 'simpledraw.js', 'rangemap.js', 'interact.js', 'base.js', 'chart-line.js', 'chart-bar.js', 'chart-tristate.js', 'chart-discrete.js', 'chart-bullet.js', 'chart-pie.js', 'chart-box.js', 'vcanvas-base.js', 'vcanvas-canvas.js', 'vcanvas-vml.js', 'footer.js'];
 
 shell.cd('src');
-
 var src = shell.cat(files).replace(/@VERSION@/mg, package.version);
 
 shell.cd('..');
